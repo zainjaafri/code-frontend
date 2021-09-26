62325d3c0325444bffd3895fd5c5fe58f216d82c
zainjaafri <zainjafri19@gmail.com>
2021-09-26 16:28:54 +0530

    login page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home-component', component: HomeComponent },
+  { path: 'login-component', component: LoginComponent },
   { path: 'app-component', component: AppComponent },
 ];
 
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email!: string;
+  password!: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  
+  login() : void {
+    if(this.email == 'admin@gmail.com' && this.password == 'admin'){
+     this.router.navigate(["home-component"]);
+    }else {
+      alert("Invalid credentials");
+    }
+  }
+  
+}
