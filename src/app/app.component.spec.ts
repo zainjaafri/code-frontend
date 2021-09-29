26eff45951e182911ff5e53552dfa864c94ff113
zainjaafri <zainjafri19@gmail.com>
2021-09-29 13:15:37 +0530

    code cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'code-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('code-frontend');
-  });
+  // it(`should have as title 'code-frontend'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('code-frontend');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
